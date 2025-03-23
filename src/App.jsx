ae2feb28c5e9879c47d4d94e6c10a00a8cace858
Artem Kuznetsov <kuzart@seznam.cz>
2025-03-23 23:08:04 +0100

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-//начало декларативного
-import { createElement } from 'react';
-import reactLogo from './assets/react.svg';
+import styles from './App.module.css';
 
 export const App = () => {
 	const currentYear = new Date().getFullYear();
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement('h1', { className: 'Heading' }, 'Hello World'),
-		createElement('img', { className: 'Logo', src: reactLogo, alt: 'Logo' }),
-		createElement(
-			'h2',
-			null,
-			'Сегодня ',
-			createElement('b', null, currentYear),
-			' год',
-		),
-	);
+	return <div className={styles.app}></div>;
 };
